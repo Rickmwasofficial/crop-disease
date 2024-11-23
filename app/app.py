3527c491ac217cf273d7a3f4415e24059ac7064f
marky376 <mmutuamark@gmail.com>
2024-11-23 08:43:25 +0300

    Added some specifications on the route (/upload)

--- app/app.py
+++ app/app.py
@@ -30,3 +30,21 @@ def upload_image():
     if file:
         file_path = os.path.join('uploads', file.filename)
         file.save(file_path)
+
+        img = image.load_img(file_path, target_size=(256, 256))
+        img_array = image.img_to_array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+
+
+        predictions = model.predict(img_array)
+        predicted_class = np.argmax(predictions, axis=1)
+        confidence = np.max(predictions)
+
+        return jsonify({
+            'class': str(predicted_class[0]),
+            'confidence': float(confidence)
+            })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
